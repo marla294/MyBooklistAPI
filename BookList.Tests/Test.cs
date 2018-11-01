8de9c6b89834c3f1152465104407a4ae941efa5a
Marla Foreman <marla294@gmail.com>
2018-11-01 15:15:42 -0500

    Adding BookList.Tests to solution

---
+++ BookList.Tests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace BookList.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
