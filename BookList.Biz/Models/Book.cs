253c96618dc45d8e6d9153495379f75a5570b613
Marla Foreman <marla294@gmail.com>
2018-11-01 16:18:35 -0500

    Creating Book class

---
+++ BookList.Biz/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace BookList.Biz.Models
+{
+    public class Book
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string URL { get; set; }
+
+        public Book() {
+            Id = 0;
+            Name = "";
+            URL = "";
+        }
+
+        public Book(int id, string name, string url) {
+            Id = id;
+            Name = name;
+            URL = url;
+        }
+    }
+}
