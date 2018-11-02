0db7d53293ffcc8c2cd1f5201b64c49b40c4d795
Marla Foreman <marla294@gmail.com>
2018-11-02 13:42:41 -0500

    Adding Cors reference

--- BookList.Service/Controllers/Books.cs
+++ BookList.Service/Controllers/Books.cs
@@ -1,10 +1,12 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Collections.Generic;
 using BookList.Biz.Models;
 using BookList.Biz.Database;
 
 namespace BookList.Service.Controllers
 {
+    [EnableCors(origins: "http://127.0.0.1:8080", headers: "*", methods: "*")]
     public class Books : ApiController
     {
         public List<Book> Get()
