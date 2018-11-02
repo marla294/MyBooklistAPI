be6db8197f93dc75ca033b37381e6e7810a15253
Marla Foreman <marla294@gmail.com>
2018-11-02 13:37:14 -0500

    Building out Books controller
    Right now this controller will just return a list of all the books in
    the database

---
+++ BookList.Service/Controllers/Books.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using System.Collections.Generic;
+using BookList.Biz.Models;
+using BookList.Biz.Database;
+
+namespace BookList.Service.Controllers
+{
+    public class Books : ApiController
+    {
+        public List<Book> Get()
+        {
+            return LoadBook.LoadAll();
+        }
+    }
+}
