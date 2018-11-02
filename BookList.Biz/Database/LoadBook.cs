87a01f135c5e91508518c070c661126260551ff5
Marla Foreman <marla294@gmail.com>
2018-11-02 12:09:48 -0500

    Creating static LoadBook class and tests
    - Added LoadByQuery method
    - Added LoadAll method
    - Added tests for LoadAll method

--- BookList.Biz/Database/LoadBook.cs
+++ BookList.Biz/Database/LoadBook.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using BookList.Biz.Models;
+
 namespace BookList.Biz.Database
 {
-    public class LoadBook
+    public static class LoadBook
     {
-        public LoadBook()
+        public static List<Book> LoadAll()
         {
+            return LoadByQuery("select * from books order by id;");
+        }
+
+        static List<Book> LoadByQuery(string sql)
+        {
+            var bookResultSet = ConnectionUtils.ExecuteCommand(new PostgreSQLConnection(), sql);
+            var bookList = new List<Book>();
+
+            for (var i = 0; i < bookResultSet[0].Count; i++)
+            {
+                Book book = Int32.TryParse(bookResultSet[0][i], out int id)
+                    ? new Book(id, bookResultSet[1][i], bookResultSet[2][i])
+                    : new Book();
+                                 
+                bookList.Add(book);
+            }
+
+            return bookList;
         }
     }
 }
---
+++ BookList.Tests/Biz/Database/TestLoadBook.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using BookList.Biz.Database;
+
+namespace BookList.Tests
+{
+    [TestFixture]
+    public class LoadBookTests
+    {
+        [Test]
+        public void TestLoadBookLoadAll()
+        {
+            var testBookList = LoadBook.LoadAll();
+
+            Assert.AreEqual(testBookList.Count, 3);
+        }
+    }
+}
