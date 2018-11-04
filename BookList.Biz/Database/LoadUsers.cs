655d73184a97b29ef1b3c50e6135da7641537acd
Marla Foreman <marla294@gmail.com>
2018-11-04 09:16:53 -0600

    Creating LoadUsers class
    To load the users out of the database

---
+++ BookList.Biz/Database/LoadUsers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using BookList.Biz.Models;
+
+namespace BookList.Biz.Database
+{
+    public static class LoadUsers
+    {
+        public static List<UserDTO> LoadAll()
+        {
+            return LoadByQuery("select * from users order by id;");
+        }
+
+        static List<UserDTO> LoadByQuery(string sql)
+        {
+            var userResultSet = ConnectionUtils.ExecuteCommand(new PostgreSQLConnection(), sql);
+            var userList = new List<UserDTO>();
+
+            for (var i = 0; i < userResultSet[0].Count; i++)
+            {
+                User user = Int32.TryParse(userResultSet[0][i], out int id)
+                    ? new User(id, userResultSet[1][i])
+                    : new User();
+
+                userList.Add(user.ToDTO());
+            }
+
+            return userList;
+        }
+    }
+}
