c7e8e9fa808d0bf47452998bc43ca738ac7a11a6
Marla Foreman <marla294@gmail.com>
2018-11-04 08:58:41 -0600

    Creating User and UserDTO models

---
+++ BookList.Biz/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace BookList.Biz.Models
+{
+    public class User
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+
+        public User()
+        {
+            Id = 0;
+            Name = "";
+        }
+
+        public User(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public UserDTO ToDTO()
+        {
+            return new UserDTO(Id, Name);
+        }
+    }
+}
---
+++ BookList.Biz/Models/UserDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BookList.Biz.Models
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public UserDTO()
+        {
+            Id = 0;
+            Name = "";
+        }
+
+        public UserDTO(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
