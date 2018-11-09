f17f5c124109c565c33e372c7f137ea668bc2269
Marla Foreman <marla294@gmail.com>
2018-11-09 17:17:58 -0600

    Name -> Title on Books

--- BookList.Biz/Models/Book.cs
+++ BookList.Biz/Models/Book.cs
@@ -3,26 +3,26 @@
     public class Book
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
         public string URL { get; set; }
         public string Author { get; set; }
 
         public Book() {
             Id = 0;
-            Name = "";
+            Title = "";
             URL = "";
             Author = "";
         }
 
         public Book(int id, string name, string url, string author) {
             Id = id;
-            Name = name;
+            Title = name;
             URL = url;
             Author = author;
         }
 
         public BookDTO ToDTO() {
-            return new BookDTO(Id, Name, URL);
+            return new BookDTO(Id, Title, URL);
         }
     }
 }
