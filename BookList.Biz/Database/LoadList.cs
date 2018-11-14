f907e4caede173d519ba68887c167f95124243bc
Marla Foreman <marla294@gmail.com>
2018-11-14 09:19:17 -0600

    Created LoadList to load the list names from the database

---
+++ BookList.Biz/Database/LoadList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using BookList.Biz.Models;
+using System.Linq;
+
+namespace BookList.Biz.Database
+{
+    public static class LoadList
+    {
+        public static List<List> LoadAll()
+        {
+            return LoadByQuery("select * from lists order by id;");
+        }
+
+        static List<List> LoadByQuery(string sql)
+        {
+            var listResultSet = ConnectionUtils.ExecuteCommand(new PostgreSQLConnection(), sql);
+            var listList = new List<List>();
+
+            for (var i = 0; i < listResultSet[0].Count; i++)
+            {
+                List list = Int32.TryParse(listResultSet[0][i], out int id)
+                    ? new List(id, listResultSet[1][i])
+                    : new List();
+
+                listList.Add(list);
+            }
+
+            return listList;
+        }
+    }
+}
