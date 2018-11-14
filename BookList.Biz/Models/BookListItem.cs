19dfaf062c2a36668fc79967ca03bd752fe055ba
Marla Foreman <marla294@gmail.com>
2018-11-14 09:06:04 -0600

    Adding List to BookListItem model

--- BookList.Biz/Models/BookListItem.cs
+++ BookList.Biz/Models/BookListItem.cs
@@ -9,6 +9,7 @@
         public int Rating { get; set; }
         public string Notes { get; set; }
         public int SortOrder { get; set; }
+        public List List { get; set; }
 
         public BookListItem() {
             Id = 0;
@@ -18,10 +19,11 @@
             Rating = 0;
             Notes = "";
             SortOrder = 0;
+            List = null;
         }
 
         public BookListItem(int id, Book book, User user, bool done, int rating, 
-                        string notes, int sortOrder) {
+                        string notes, int sortOrder, List list) {
             Id = id;
             Book = book;
             User = user;
@@ -29,6 +31,7 @@
             Rating = rating;
             Notes = notes;
             SortOrder = sortOrder;
+            List = list;
         }
 
         public BookListItemDTO ToDTO()
