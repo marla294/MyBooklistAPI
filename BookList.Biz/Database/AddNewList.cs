a02c8869cb73fc4351b1a2cbd662119e0e6a6f1c
Marla Foreman <marla294@gmail.com>
2018-11-20 16:19:21 -0600

    Inserting new list into lists table and booklist table

--- BookList.Biz/Database/AddNewList.cs
+++ BookList.Biz/Database/AddNewList.cs
@@ -11,6 +11,13 @@ namespace BookList.Biz.Database
         {
             var sql = "insert into lists (name) values ('New List')";
             ConnectionUtils.ExecuteCommand(new PostgreSQLConnection(), sql);
+
+            sql = "select id from lists order by id desc limit 1";
+            var id = ConnectionUtils.ExecuteCommand(new PostgreSQLConnection(), sql)[0][0];
+
+            sql = "insert into booklist(book, username, done, rating, notes, " +
+                $"sortorder, list) values (null, 1, false, null, '', 0, {id})";
+            ConnectionUtils.ExecuteCommand(new PostgreSQLConnection(), sql);
         }
     }
 }
