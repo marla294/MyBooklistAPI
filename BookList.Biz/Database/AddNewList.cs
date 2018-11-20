8e216c9b6f2c4831132b82f79d1e83c025df3181
Marla Foreman <marla294@gmail.com>
2018-11-20 15:04:03 -0600

    Adding "AddNewList" class

---
+++ BookList.Biz/Database/AddNewList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using BookList.Biz.Models;
+using System.Linq;
+
+namespace BookList.Biz.Database
+{
+    public static class AddNewList
+    {
+        public static void AddNew()
+        {
+            var sql = "insert into lists (name) values ('New List')";
+            ConnectionUtils.ExecuteCommand(new PostgreSQLConnection(), sql);
+        }
+    }
+}
