26c8018396c988d58b811edd923936ee6172370c
Marla Foreman <marla294@gmail.com>
2018-11-30 11:37:46 -0600

    Created tests for PostgreSQL stuff

---
+++ BookList.Tests/Biz/Database/PostgreSQL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+using BookList.Biz.Database;
+
+namespace BookList.Tests.Biz.Database
+{
+    [TestFixture]
+    public class PostgreSQL
+    {
+        [Test]
+        public void TestTake()
+        {
+
+        }
+    }
+}
