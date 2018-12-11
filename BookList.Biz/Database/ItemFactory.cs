5b7d6a9918516113f4b23474943fa635366fc9cc
Marla Foreman <marla294@gmail.com>
2018-12-11 14:34:29 -0600

    Adding tests for CreateNewItem, discovered bug 🐛
    Issue with the SQL for inserting into booklist table.

--- BookList.Biz/Database/ItemFactory.cs
+++ BookList.Biz/Database/ItemFactory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using BookList.Biz.Models;
+using System.Linq;
 
 namespace BookList.Biz.Database
 {
@@ -22,17 +23,20 @@ namespace BookList.Biz.Database
         public static List<BookListItem> LoadAll(IDbConnection dbConnection)
         {
             var itemResultSet = dbConnection.Take("booklist").OrderBy("id").Execute();
-
             var itemList = new List<BookListItem>();
 
             for (var i = 0; i < itemResultSet[0].Count; i++)
             {
                 BookListItem item = Int32.TryParse(itemResultSet[0][i], out int id) ? LoadOneItem(itemResultSet, i) : new BookListItem();
-
                 itemList.Add(item);
             }
 
             return itemList;
+        }
+
+        public static BookListItem LoadSingle(IDbConnection dbConnection, int id)
+        {
+            return LoadAll(dbConnection).FirstOrDefault<BookListItem>(item => item.Id == id);
         }
 
         static BookListItem LoadOneItem(List<List<string>> itemResultSet, int row) {
--- BookList.Tests/Biz/Database/ItemCRUDOperations.cs
+++ BookList.Tests/Biz/Database/ItemCRUDOperations.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using BookList.Biz.Database;
 
 namespace BookList.Tests.Biz.Database
@@ -6,6 +7,33 @@ namespace BookList.Tests.Biz.Database
     [TestFixture]
     public class ItemCRUDOperations
     {
+        [Test]
+        public void TestCreateNewItem()
+        {
+            var db = new PostgreSQLConnection();
+
+            Int32.TryParse(BookFactory.CreateNewBook(db, "test book", "test author"), out int bookId);
+            Int32.TryParse(ListFactory.CreateNewList(db, "test list"), out int listId);
+
+            if (Int32.TryParse(ItemFactory.CreateNewItem(db, bookId, listId), out int id))
+            {
+                var testItem = ItemFactory.LoadSingle(db, id);
+
+                Assert.IsNotNull(testItem);
+                Assert.AreEqual(bookId, testItem.Book.Id);
+                Assert.AreEqual(listId, testItem.ListId);
+
+                //ItemFactory.DeleteItem(db, id);
+
+                BookFactory.DeleteBook(db, bookId);
+                ListFactory.DeleteList(db, listId);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+
         [Test]
         public void TestLoadAll()
         {
