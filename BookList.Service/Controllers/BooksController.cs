c3d1ff876385c7c7ff05d25614eef50b0a974ea6
Marla Foreman <marla294@gmail.com>
2018-12-11 14:05:09 -0600

    Adding Post and Delete to BooksController

--- BookList.Service/Controllers/BooksController.cs
+++ BookList.Service/Controllers/BooksController.cs
@@ -4,14 +4,38 @@ using System.Collections.Generic;
 using BookList.Biz.Models;
 using BookList.Biz.Database;
 
+public class BookFromBody
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+}
+
 namespace BookList.Service.Controllers
 {
     [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
     public class BooksController : ApiController
     {
+        PostgreSQLConnection Db { get; set; }
+
+        public BooksController()
+        {
+            Db = new PostgreSQLConnection();
+        }
+
         public List<Book> Get()
         {
-            return BookFactory.LoadAll(new PostgreSQLConnection());
+            return BookFactory.LoadAll(Db);
+        }
+
+        // returns the id of the new list as a string
+        public string Post([FromBody]BookFromBody value)
+        {
+            return BookFactory.CreateNewBook(Db, value.Title, value.Author);
+        }
+
+        public void Delete(int id)
+        {
+            BookFactory.DeleteBook(Db, id);
         }
     }
 }
