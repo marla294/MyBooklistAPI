b9428d0bf9273d2efe7b8616c5b60d6ab60b3499
Marla Foreman <marla294@gmail.com>
2018-12-28 15:30:37 -0600

    Adding CreateNewUser to the Users Controller

--- BookList.Service/Controllers/UsersController.cs
+++ BookList.Service/Controllers/UsersController.cs
@@ -4,14 +4,34 @@ using System.Collections.Generic;
 using BookList.Biz.Models;
 using BookList.Biz.Database;
 
+public class UserData
+{
+    public string Name { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
+
 namespace BookList.Service.Controllers
 {
     [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
     public class UsersController : ApiController
     {
+        PostgreSQLConnection Db { get; set; }
+
+        public UsersController()
+        {
+            Db = new PostgreSQLConnection();
+        }
+
         public List<User> Get()
         {
-            return UserFactory.LoadAll(new PostgreSQLConnection());
+            return UserFactory.LoadAll(Db);
+        }
+
+        // returns the id of the new list as a string
+        public string Post([FromBody]UserData value)
+        {
+            return UserFactory.CreateNewUser(Db, value.Name, value.Username, value.Password);
         }
     }
 }
