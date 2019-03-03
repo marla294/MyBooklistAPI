44a0167a8704aa3085ec3738e9e9bbdf9aec6de6
Marla Foreman <marla294@gmail.com>
2019-03-02 22:22:21 -0600

    Created CRUDOperations.cs, a class that will handle the CRUD stuff every class has to do

---
+++ BookList.Biz/Database/CRUDOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookList.Biz.Database
+{
+    public class CRUDOperations
+    {
+        public int Create(IDbConnection db, string table, string col, object val)
+        {
+            db.Insert(table, new KeyValuePair<string, object>[] {
+                                Pairing.Of(col, val)
+            }).Execute();
+
+            return Int32.TryParse(db.Take("lists").OrderBy("id", "desc").Limit(1).Execute()[0][0], out int id) ? id : 0;
+        }
+    }
+}
